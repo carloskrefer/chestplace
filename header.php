fd393e7231c85bab3cd86cd48c195364591b51ae
Gabriel Martins <gabrielgmns071204@gmail.com>
2023-04-18 11:27:13 -0300

    Criado estruturação do formulário de cadastro de produto

---
+++ header.php
@@ -0,0 +1,20 @@
+<!-- Top menu on small screens -->
+<header class="w3-bar w3-top w3-hide-large w3-black w3-xlarge">
+  <div class="w3-bar-item w3-padding-24 w3-wide">LOGO</div>
+  <a href="javascript:void(0)" class="w3-bar-item w3-button w3-padding-24 w3-right" onclick="w3_open()"><i class="fa fa-bars"></i></a>
+</header>
+
+ <!-- Top header -->
+ <header class="w3-container w3-xlarge">
+
+<h3 class="w3-wide w3-left w3-padding-16"><b>LOGO</b></h3>
+<p class="w3-right">
+    <a class="no-underline" href="#" target="" >
+        <spam class="w3-large" style="text-decoration: underline;">Gabriel</spam>
+        <i class="fa fa-user w3-margin-right"></i>
+    </a>
+    <a class="no-underline" href="#">
+        <i class="fa fa-solid fa-plus"></i>
+    </a>
+</p>
+</header>
