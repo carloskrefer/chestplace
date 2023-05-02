e2c62dbc80cc6f3e4a1e9cfb5a14f6473b257ac5
Gabriel <gabrielgmns071204@gmail.com>
2023-05-02 00:27:40 -0300

    Vendedor agora é cadastrado no bd v0.1a

--- actions/cadVendedor_exe.php
+++ actions/cadVendedor_exe.php
@@ -1,72 +1,41 @@
 <?php
     session_start();
 
-
-
     include("../common/functions.php");
     include("../database/conectaBD.php");
 
-    $titulo         = $_POST["email"];
-    $descricao      = $_POST["descricao"];
-    $preco          = $_POST["preco"];
-    $dataPublicacao = DateTime::createFromFormat('Y-m-d\TH:i', $_POST["dataPublicacao"]);
-    $marca          = $_POST["marca"];
-    $conservacao    = $_POST["conservacao"];
+    $nome = $_POST["nome"];
+    $email = $_POST["email"];
+    $senha = $_POST["senha"];
+    $nomeEstabelecimento = $_POST["nomeEstabelecimento"];
+    $cnpj = $_POST["cnpj"];
+    $cpf  = $_POST["cpf"];
 
     // echo $_POST["dataPublicacao"];
-    $insertQuery = "INSERT INTO camiseta(titulo, descricao, preco, conservacao, data_hora_publicacao, id_vendedor, id_marca) VALUES(
-        \"".$titulo."\",
-        \"".$descricao."\",
-        \"".$preco."\",
-        \"".$conservacao."\",
-        \"".$dataPublicacao->format('Y-m-d H:i:s')."\",
-        ".$_SESSION["idVendedor"].",
-        ".$marca."
-    );";
-    echo $insertQuery;
-    if (mysqli_query($conn, $insertQuery)){
-        echo "<script>alert(\"Cadastro realizado\");</script>";
-        
-        $selectIdCamiseta = "SELECT id FROM camiseta ORDER BY id LIMIT 1;";
+    $insertQueryUsuario = "INSERT INTO usuario (nome, email, senha) VALUES (\"".$nome."\",\"".$email."\",\"".$senha."\")";
     
-        $result = mysqli_query($conn, $selectIdCamiseta);
+    echo $insertQueryUsuario;
+    if (mysqli_query($conn, $insertQueryUsuario)){
+        cLog("usuario cadastrado");
 
-        if (mysqli_num_rows($result) > 0){
-            while($row = mysqli_fetch_assoc($result)){
-                $idCamiseta = $row["id"];
+        $selectIdUsuario = "SELECT MAX(id) id FROM USUARIO";
+
+        $idUsuario = mysqli_query($conn, $selectIdUsuario);
+
+        if (mysqli_num_rows($idUsuario) > 0) {
+            while($usuario = mysqli_fetch_assoc($idUsuario)) {
+                $insertQueryVendedor = "INSERT INTO vendedor (id_usuario, nome_estabelecimento, cpf, cnpj, id_endereco) 
+                VALUES (\"".$usuario["id"]."\",\"".$nomeEstabelecimento."\",\"".$cpf."\",\"".$cnpj."\",1)";
             }
         }
-    
-        // Loop através de cada arquivo enviado pelo formulário
-        foreach ($_FILES["imagem"]["tmp_name"] as $key => $tmp_name) {
-            // Verifica se o arquivo é uma imagem
-            $file_type = $_FILES["imagem"]["type"][$key];
-            if (strpos($file_type, "image/") === 0) {
-                // Lê o conteúdo do arquivo de imagem
-                $filename = $_FILES["imagem"]["name"][$key];
-                $file_size = $_FILES["imagem"]["size"][$key];
-                $file_tmp = $_FILES["imagem"]["tmp_name"][$key];
-                $handle = fopen($file_tmp, "r");
-                $content = fread($handle, $file_size);
-                fclose($handle);
-                // Escapa os caracteres especiais do conteúdo
-                $content = mysqli_real_escape_string($conn, $content);
-                // Insere o conteúdo no banco de dados
-                $sql = "INSERT INTO imagem (id_produto, imagem) VALUES(\"".$idCamiseta."\",\"".$content."\")";
-                
-                echo "<script>console.log(\"".$filename."\");</script>";
-    
-                if (mysqli_query($conn, $sql)){
-                    echo "<script>console.log(\"Cadastro de imagem realizado\");</script>";
-                }
-                else
-                    echo "<script>console.log(\"Erro ao inserir imagem no banco de dados:" . mysqli_error($conn) . "\");</script>";
-                
-            }
+
+        if (mysqli_query($conn, $insertQueryVendedor)){
+            alert("Vendedor cadastrado");
         }
+        redirect("../index.php");
     }
     else
-        echo "<script>alert(\"Erro ao cadastrar produto:" . mysqli_error($conn) . "\");</script>";
+       alert("Erro ao cadastrar usuário!");
 
 ?>
 
--- cadastro_vendedor.php
+++ cadastro_vendedor.php
@@ -16,41 +16,50 @@
     <script src="../scripts/formFunctions.js"></script>
 </head>
 
-<div class="w3-main w3-container">
-	<div class="w3-panel w3-padding-large w3-card-4 w3-light-grey">
-		<p class="w3-large">
-		<div class="w3-code cssHigh notranslate" style="border-left:4px solid blue;">
-			<div class="w3-container w3-theme">
-				<h2>Cadastro de Vendedor</h2>
-			</div>
-
-            <div>
-                <td>
-                    <input class=\"w3-input w3-border w3-light-grey\" name=Nome type=\"text\" placeholder=Nome required></div>
-                </td>
-            <div>
-                <td>
-                    <input class=\"w3-input w3-border w3-light-grey\" name=\Email type=\"text\" placeholder=Email required></div>
-                </td>
-            <div>
-                <td>
-                    <input class=\"w3-input w3-border w3-light-grey\" name=\CPF type=\"text\" placeholder=CPF required></div>
-                </td>
-            <div>
-                <td>
-                <input class=\"w3-input w3-border w3-light-grey\" name=\CNPJ type=\"text\" placeholder=CNPJ required></div>
-                </td>
-            <div>
-                <td>
-                    <input class=\"w3-input w3-border w3-light-grey\" name=\Senha type=\"text\" placeholder=Senha required></div>
-                </td>
-
-
-            <p>
-                <input type="submit" value="Cadastrar" class="w3-btn w3-red">
-
+<body>
+    <?php require './common/header.php'; ?>
+    <div class="w3-main w3-container">
+        <div class="w3-panel w3-padding-large w3-card-4 w3-light-grey">
+            <p class="w3-large">
+                <div class="w3-code cssHigh notranslate" style="border-left:4px solid blue;">
+                    <div class="w3-container w3-theme">
+                        <h2>Cadastro de Vendedor</h2>
+                    </div>
+                    <form id="cadForm"class="w3-container" action="./actions/cadVendedor_exe.php" method="post" enctype="multipart/form-data" onsubmit="return validarFormulario();">
+                        <div>
+                            <td>
+                                <input class="w3-input w3-border w3-light-grey" name="nome" type="text" placeholder="Nome" required></div>
+                            </td>
+                        <div>
+                        <div>
+                            <td>
+                                <input class="w3-input w3-border w3-light-grey" name="nomeEstabelecimento" type="text" placeholder="Nome do estabelecimento" required></div>
+                            </td>
+                        <div>
+                            <td>
+                                <input class="w3-input w3-border w3-light-grey" name="email" type="text" placeholder="Email" required></div>
+                            </td>
+                        <div>
+                            <td>
+                                <input class="w3-input w3-border w3-light-grey" name="cpf" type="text" placeholder="CPF" required></div>
+                            </td>
+                        <div>
+                            <td>
+                            <input class="w3-input w3-border w3-light-grey" name="cnpj" type="text" placeholder="CNPJ" required></div>
+                            </td>
+                        <div>
+                            <td>
+                                <input class="w3-input w3-border w3-light-grey" name="senha" type="text" placeholder="Senha" required></div>
+                            </td>
+    
+                        <p>
+                            <input type="submit" value="Cadastrar" class="w3-btn w3-red">
+                        </p>
+                    </form>
+                </div>
             </p>
-            <h2>Já possuo Conta:</h2>
-            <div class ='justify-center'>
-              <button onclick="https:" >Ir para o Login</button>
+        </div>
+    </div>
+</body>
+</html>
 
