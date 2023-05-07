968569591cc7f32f90f82931f69260d66b345070
Gabriel <gabrielgmns071204@gmail.com>
2023-05-07 13:05:40 -0300

    Tratamento de erros de conexão com BD

--- database/conectaBD.php
+++ database/conectaBD.php
@@ -9,6 +9,7 @@ Agosto/2022
 ---------------------------------------------------------------------------------->
 
 <?php
+
     global $servername ;
     global $username;
     global $password;
@@ -19,5 +20,20 @@ Agosto/2022
     $password = "";
     $database = "chestplace";
 
-    $conn = mysqli_connect($servername, $username, $password, $database);
+    try{
+        $conn = mysqli_connect($servername, $username, $password, $database);
+    
+        if($conn){
+            echo "<script>console.log(\"Conexão com banco de dados realizada com sucesso.\")</script>";
+        } else {
+            echo "<h1>Ocorreu um erro.</h1>";
+            echo "<p>Houve um erro ao se conectar com o banco de dados. Por favor, tente novamente mais tarde.</p>";
+            echo "<script>console.log(\"".(mysqli_connect_error())."\")</script>";
+        }
+    }
+    catch(mysqli_sql_exception $e){
+        echo "<h1>Ocorreu um erro.</h1>";
+        echo "<p>Houve um erro ao se conectar com o banco de dados. Por favor, tente novamente mais tarde.</p>";
+        echo "<script>console.log(\"".($e->getMessage())."\")</script>";
+    }
 ?>
