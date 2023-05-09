6d290d289961202e300c21eb71f38dd128383235
Gabriel <gabrielgmns071204@gmail.com>
2023-05-09 17:31:03 -0300

    Descrição função consultarCEP(cep)

--- scripts/viaCep/viaCep.js
+++ scripts/viaCep/viaCep.js
@@ -1,3 +1,9 @@
+/**
+ * Consulta os dados de um CEP através da API do ViaCEP.
+ *
+ * @param string $cep O CEP a ser consultado no sistema.
+ * @return array|null Os dados do CEP retornados pela API do ViaCEP ou null em caso de erro.
+ */
 async function consultarCEP(cep) {
     try {
       const response = await fetch(`https://viacep.com.br/ws/${cep}/json/`);
