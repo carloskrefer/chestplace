a0e2c7d014bfa8f638433c8517691c2bd742721f
Carlos Krefer <carloskrefer@hotmail.com>
2023-05-10 11:35:05 -0300

    Remove banner desnecessário

--- index.php
+++ index.php
@@ -155,16 +155,6 @@ body { background-color: #cca310; }
     </div>
   </div>
 
-  <!-- Image header -->
-  <div style = "background-color: #cca310" class="w3-display-container w3-container">
-    <img src="./w3images/jeans.jpg" alt="Jeans" style="width:100%">
-    <div class="w3-display-topleft w3-text-white" style="padding:24px 48px">
-      <h1 class="w3-jumbo w3-hide-small">As melhores camisetas</h1>
-      <h1 class="w3-hide-large w3-hide-medium">Novas e Usadas</h1>
-      <h1 class="w3-hide-small">Confira abaixo</h1>
-      <p><a href="#jeans" class="w3-button w3-black w3-padding-large w3-large">SHOP NOW</a></p>
-    </div>
-  </div>
   <div class="w3-container w3-text-grey" id="jeans">
     <p>Produtos</p>
   </div >
