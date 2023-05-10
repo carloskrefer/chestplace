6e80c5901bf5580c1c9257714a9b2c642dc43aee
Gabriel <gabrielgmns071204@gmail.com>
2023-05-10 11:52:34 -0300

    cadvendedor_exe usa try-catch e pega ids por mysqli_insert_iD()

--- actions/cadVendedor_exe.php
+++ actions/cadVendedor_exe.php
@@ -4,78 +4,66 @@
     include("../common/functions.php");
     include("../database/conectaBD.php");
 
+    // Dados do usuario
     $nome = $_POST["nome"];
-    $email = $_POST["email"];
+    $emailLogin = $_POST["emailLogin"];
     $senha = $_POST["senha"];
+
+    // Dados do vendedor
     $nomeEstabelecimento = $_POST["nomeEstabelecimento"];
-    // $cnpj = $_POST["cnpj"];
-    $cpf  = $_POST["cpf"];
+    $cnpj                = (strlen($_POST["cpfCnpj"]) == 18) ? $_POST["cpfCnpj"] : "null";
+    $cpf                 = (strlen($_POST["cpfCnpj"]) == 14) ? $_POST["cpfCnpj"] : "null";
+    $emailContato        = $_POST["emailContato"];
+    $telefoneContato     = $_POST["telefoneContato"];
 
-    $rua = $_POST["rua"];
-    $cep = $_POST["cep"];
+    // Dados de endereço
+    $cep         = $_POST["cep"];
+    $rua         = $_POST["rua"];
+    $numero      = $_POST["numero"];
+    $bairro      = $_POST["bairro"];
     $complemento = $_POST["complemento"];
-    $numero = $_POST["numero"];
-    $bairro = $_POST["bairro"];
-    $cidade = $_POST["cidade"];
-    $estado = $_POST["estadoSelect"];
+    $cidade      = $_POST["cidade"];
+    $estado      = $_POST["estadoSelect"];
 
-    $insertQueryEndereco = "INSERT INTO endereco (rua, cep, complemento, numero, bairro, cidade, uf) VALUES (\"$rua\",\" $cep\", \"$complemento\", \"$numero\", \"$bairro\",\" $cidade\", \"$estado\");";
+    // Query para inserir endereço no BD
+    $insertQueryEndereco = "INSERT INTO endereco (rua, cep, complemento, numero, bairro, cidade, uf) VALUES (\"$rua\",\"$cep\", \"$complemento\", \"$numero\", \"$bairro\",\"$cidade\", \"$estado\");";
 
-    $selectQueryEndereco = "SELECT MAX(id) id_end FROM endereco";
-
-    // echo $_POST["dataPublicacao"];
-    $insertQueryUsuario = "INSERT INTO usuario (nome, email, senha) VALUES (\"".$nome."\",\"".$email."\",\"".$senha."\")";
+    // Query para inserir usuario no BD
+    $insertQueryUsuario = "INSERT INTO usuario (nome, email, senha) VALUES (\"".$nome."\",\"".$emailLogin."\",\"".$senha."\")";
     
-    // echo $insertQueryUsuario;
-
-    echo $insertQueryEndereco;
-
-    if(mysqli_query($conn, $insertQueryEndereco)){
-
-        $idEndereco = mysqli_query($conn, $selectQueryEndereco);
-
-        if(mysqli_num_rows($idEndereco) > 0){
     
-            if (mysqli_query($conn, $insertQueryUsuario)){
-                
-                cLog("usuario cadastrado");
+    // Inicia transaction
+    mysqli_begin_transaction($conn);
+
+    // Tenta realizar INSERTs
+    try{
         
-                $selectIdUsuario = "SELECT MAX(id) id FROM USUARIO";
+        // Inserir endereço
+        mysqli_query($conn, $insertQueryEndereco);
+
+        // Inserir usuário
+        $idEndereco = mysqli_insert_id($conn);
+        mysqli_query($conn, $insertQueryUsuario);
+
+        // Inserir vendedor
+        $idUsuario = mysqli_insert_id($conn);
+        $insertQueryVendedor = " 
+        INSERT INTO vendedor (id_usuario, nome_estabelecimento, cpf, id_endereco, email_contato, telefone_contato)
+        VALUES (\"".$idUsuario."\",\"".$nomeEstabelecimento."\",\"".$cpf."\", ".$idEndereco.", \"".$emailContato."\",\"".$telefoneContato."\");";
+        mysqli_query($conn, $insertQueryVendedor);
+
+        // Informar que inserções foram feitas com sucesso
+        alert("Usuário cadastrado com sucesso!");
+        mysqli_commit($conn); // Termina transaction
+
+
+    } catch(Exception $e) { // Caso haja algum erro inserindo os dados 
         
-                $idUsuario = mysqli_query($conn, $selectIdUsuario);
-        
-                if (mysqli_num_rows($idUsuario) > 0) {
-
-                    while($usuario = mysqli_fetch_assoc($idUsuario)) {
-
-                        while($end = mysqli_fetch_assoc($idEndereco)){
-                            
-                            $insertQueryVendedor = " 
-                            INSERT INTO vendedor (id_usuario, nome_estabelecimento, cpf, id_endereco)
-                            VALUES (\"".$usuario["id"]."\",\"".$nomeEstabelecimento."\",\"".$cpf."\", ".$end["id_end"].");
-                            ";
-
-                        }
-
-                    }
-                    
-                }
-        
-                if (mysqli_query($conn, $insertQueryVendedor)){
-                    alert("Vendedor cadastrado");
-                }
-                redirect("../index.php");
-            }
-            else
-               alert("Erro ao cadastrar usuário!");
-
-            
-
-        }
-        
+        mysqli_rollback($conn); // Desfazer transaction
+        alert("Houve um erro ao cadastrar o usuário. Tente novamente mais tarde.");
     }
-
     
-
+    redirect("../index.php");
+    
 ?>
 
