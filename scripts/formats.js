edb978d349c250ab0c0632f0a59ab0e1937d374a
Gabriel <gabrielgmns071204@gmail.com>
2023-05-12 17:59:27 -0300

    documentação de funções de formatação

--- scripts/formats.js
+++ scripts/formats.js
@@ -1,4 +1,19 @@
+/**
+ * Formata um número identificador passado e, dependendo do tamanho do identificador
+ * formata para CPF ou CNPJ.
+ * - Caso o tamanho não seja de nenhum dos dois, o valor original é retornado
+ * 
+ * @param {string} cpf - O número de CPF a ser formatado.
+ * @returns {string} - O CPF formatado.
+ *
+ * @example
+ * formatarCPFCNPJ('12345678900');    // Retorna '123.456.789-00'
+ * formatarCPFCNPJ('01234567891234'); // Retorna '01.234.567/8912-34'
+ * formatarCPFCNPJ('01234');          // Retorna '01234'
+ * 
+ */
 function formatarCPFCNPJ(identificador){
+
     // Remove todos os caracteres não numéricos
     const numeros = identificador.replace(/\D/g, '');
 
@@ -12,14 +27,16 @@ function formatarCPFCNPJ(identificador){
 
 
 /**
- * Formata um número de CPF para o formato XXX.XXX.XXX-XX.
+ * Formata um número passado para o formato XXX.XXX.XXX-XX [CPF].
+ * 
  * @param {string} cpf - O número de CPF a ser formatado.
- * @returns {string} - O CPF formatado.
+ * @returns {string}   - O CPF formatado.
  *
- * Exemplo:
+ * @example
  * formatarCPF('12345678900'); // Retorna '123.456.789-00'
  */
 function formatarCPF(cpf) {
+
     // Remove todos os caracteres não numéricos
     const numeros = cpf.replace(/\D/g, '');
   
@@ -31,23 +48,47 @@ function formatarCPF(cpf) {
 
 
 /**
- * Formata um número de CNPJ para o formato XX.XXX.XXX/XXXX-XX.
+ * Formata um número passado para o formato XX.XXX.XXX/XXXX-XX [CNPJ].
+ * 
  * @param {string} cnpj - O número de CNPJ a ser formatado.
- * @returns {string} - O CNPJ formatado.
+ * @returns {string}    - O CNPJ formatado.
  *
- * Exemplo:
+ * @example
  * formatarCNPJ('12345678000190'); // Retorna '12.345.678/0001-90'
  */
 function formatarCNPJ(cnpj){
+
+    // Remove todos os caracteres não numéricos
     const numeros = cnpj.replace(/\D/g, '');
     
-    return numeros.replace(/^(\d{2})(\d{3})(\d{3})(\d{4})(\d{2})$/, '$1.$2.$3/$4-$5')
+    // Aplica a formatação usando Regex
+    return numeros.replace(/^(\d{2})(\d{3})(\d{3})(\d{4})(\d{2})$/, '$1.$2.$3/$4-$5');
 } 
 
 
 
 
-
+/**
+ * Formata um número passado para algum dos seguintes formatos
+ * dependendo da quantidade de caracteres:
+ * -> tamanho:10; (XX) XXXX-XXXX
+ * -> tamanho:8;  XXXX-XXXX
+ * -> tamanho:11 + telefone inicia com '0800'; 0800 123 456
+ * -> tamanho:11; (XX) X XXXX-XXXX
+ * 
+ * - Caso não esteja em nenhum desses tamanhos, são retornados apenas os números do telefone passado
+ * 
+ * @param   {string} telefone - O número de telefone a ser formatado.
+ * @returns {string}          - O telefone formatado.
+ *
+ * @example
+ * formatarTelefone('0123456789');  // Retorna '(01) 2345-6789'
+ * formatarTelefone('12345678');    // Retorna '1234-5678'
+ * formatarTelefone('08001234567'); // Retorna '0800 123 4567'
+ * formatarTelefone('01234567890'); // Retorna '(01) 2 3456-7890'
+ * formatarTelefone('01234');       // Retorna '01234'
+ * 
+ */
 function formatarTelefone(telefone){
     telefone = telefone.replace(/\D/g, "");
 
@@ -73,7 +114,7 @@ function formatarTelefone(telefone){
  * @param {string} telefone - O número de telefone fixo a ser formatado.
  * @returns {string} - O telefone fixo formatado.
  *
- * Exemplo:
+ * @example
  * formatarTelefoneFixo('1123456789'); // Retorna '(11) 2345-6789'
  */
 function formatarTelefoneFixo(telefone){
@@ -88,7 +129,7 @@ function formatarTelefoneFixo(telefone){
  * @param {string} telefone - O número de telefone único 400x a ser formatado.
  * @returns {string} - O telefone único 400x formatado.
  *
- * Exemplo:
+ * @example
  * formatarTelUnic400X('12345678'); // Retorna '1234-5678'
  */
 function formatarTelUnic400X(telefone){
@@ -103,7 +144,7 @@ function formatarTelUnic400X(telefone){
  * @param {string} telefone - O número de telefone 0800 a ser formatado.
  * @returns {string} - O telefone 0800 formatado.
  *
- * Exemplo:
+ * @example
  * formatarTel0800('08001234567'); // Retorna '0800 123 456'
  */
 function formatarTel0800(telefone){
@@ -118,7 +159,7 @@ function formatarTel0800(telefone){
  * @param {string} telefone - O número de telefone comum a ser formatado.
  * @returns {string} - O telefone comum formatado.
  *
- * Exemplo:
+ * @example
  * formatarTelComum('11987654321'); // Retorna '(11) 9 8765-4321'
  */
 function formatarTelComum(telefone){
@@ -133,7 +174,7 @@ function formatarTelComum(telefone){
  * @param {string} cep - O número de CEP a ser formatado.
  * @returns {string} - O CEP formatado.
  *
- * Exemplo:
+ * @example
  * formatarCEP('12345678'); // Retorna '12345-678'
  */
 function formatarCEP(cep){
@@ -174,12 +215,18 @@ function formatarQuantidade(quantidade){
 
 
 
-
+/**
+ * Configura o preço em um campo de input.
+ * 
+ * @param {HTMLInputElement} input - O campo de input que contém o preço.
+ * 
+ * @returns {void}
+ *
+ */
 function configurarPreco(input){
 
-    if (input.value == ""){
-        input.value = 0.00; 
-    }
+    // Se não houver valor inserido, definir como '0.00'
+    if (input.value == ""){ input.value = "0.00"; }
 
     // Armazena a posição atual do cursor
     input.selectionStart = input.selectionEnd = input.value.length-3;
@@ -189,7 +236,6 @@ function configurarPreco(input){
 
     // Formata o valor com duas casas decimais, se possível
     var valor = parseFloat(input.value.replace(',', '.'));
-
 
     if (!isNaN(valor) && input.value.trim() !== '') {
         valor = valor/100;
