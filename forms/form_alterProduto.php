8f28dadd5d9b203d18822b684eba06bf756faeea
Gabriel <gabrielgmns071204@gmail.com>
2023-05-12 14:49:33 -0300

    alterProduto com arquivo .js para scripts

--- forms/form_alterProduto.php
+++ forms/form_alterProduto.php
@@ -42,9 +42,16 @@
 	<link rel="stylesheet" href="css/customize.css">
     <link rel="stylesheet" href="../styles.css">
     <script src="../scripts/jQuery/jquery-3.6.4.min.js"></script>
-    <script src="../scripts/script_alterProduto.js"></script>
+    <script src="../scripts/formats.js"></script>
     <script src="../scripts/formFunctions.js"></script>
-    <script src="../scripts/validacoesProduto.js"></script>
+    <script>
+        /**
+         * -----------INIT-----------
+         */
+        // Deleção de imagens
+        var idImagensDeletar = [];
+        var idImagensOriginal= [];
+    </script>
 
 </head>
 
@@ -172,7 +179,7 @@
                                                                 <tr>
                                                                     <td class=\"w3-center\"     ><input class=\"tamanho\" onclick=\"checkTamanho(this,'inpt-".$rowTamanho["codigo"]."')\" type=\"checkbox\" name=\"tamanho[]\" id=\"tamanho\" class=\"tamanho\" checked></td>
                                                                     <td class=\"w3-left-align\" >".$rowTamanho["codigo"]." - ".$rowTamanho["descricao"]."</td>
-                                                                    <td class=\"w3-center\"     ><input class=\"quantidade\" id=\"inpt-".$rowTamanho["codigo"]."\" name=\"quantidade_".$rowTamanho["codigo"]."\" type=\"number\" style=\"width:50%; text-align:center\" min=0 step=\"1\" oninput=\"configurarQtde(this)\" onblur=\"configurarQtde(this)\" value=\"".$rowEstq["quantidade"]."\"required></td>
+                                                                    <td class=\"w3-center\"     ><input class=\"quantidade\" id=\"inpt-".$rowTamanho["codigo"]."\" name=\"quantidade_".$rowTamanho["codigo"]."\" type=\"text\" style=\"width:50%; text-align:center\" min=0 step=\"1\" oninput=\"this.value = formatarQuantidade(this.value)\" onblur=\"this.value = formatarQuantidade(this.value)\" value=\"".$rowEstq["quantidade"]."\"required></td>
                                                                 </tr>
                                                             ";
                                                         }
@@ -181,7 +188,7 @@
                                                             <tr>
                                                                 <td class=\"w3-center\"     ><input class=\"tamanho\" onclick=\"checkTamanho(this,'inpt-".$rowTamanho["codigo"]."')\" type=\"checkbox\" name=\"tamanho[]\" id=\"tamanho\" class=\"tamanho\"></td>
                                                                 <td class=\"w3-left-align\" >".$rowTamanho["codigo"]." - ".$rowTamanho["descricao"]."</td>
-                                                                <td class=\"w3-center\"     ><input class=\"quantidade\" id=\"inpt-".$rowTamanho["codigo"]."\" name=\"quantidade_".$rowTamanho["codigo"]."\" type=\"number\" style=\"width:50%; text-align:center\" min=0 step=\"1\" oninput=\"configurarQtde(this)\" onblur=\"configurarQtde(this)\" disabled required></td>
+                                                                <td class=\"w3-center\"     ><input class=\"quantidade\" id=\"inpt-".$rowTamanho["codigo"]."\" name=\"quantidade_".$rowTamanho["codigo"]."\" type=\"text\" style=\"width:50%; text-align:center\" min=0 step=\"1\" oninput=\"this.value = formatarQuantidade(this.value)\" onblur=\"this.value = formatarQuantidade(this.value)\" disabled required></td>
                                                             </tr>
                                                         ";
                                                     }
@@ -304,159 +311,8 @@
 	</div>
 
 
-    <script>
-        // Preview de imagens
-        let inputImagem      = document.getElementById("Imagem");
-        let tablePrevImagens = document.getElementById("previewTable");
-        let tbodyPrevImagens =tablePrevImagens.querySelector("tbody");
-
-        inputImagem.addEventListener("change", () => exibirPreviaImagens());
-
-        // Deleção de imagens
-        var idImagensDeletar = [];
-
-        function enviarFormulario(){
-            if(
-                validarQuantidadeImagensDeletadas(idImagensOriginal, idImagensDeletar)&&
-                validarFormulario()
-                )
-            {
-                let formulario = document.getElementById("frmAlterProduto");
-                let formData = new FormData(formulario);
-
-                formData.append('idImagensDeletar', JSON.stringify(idImagensDeletar));
-
-                enviarFormularioPost(formulario.action,formData)
-                .then( response => {
-                    alert("Anúncio atualizado com sucesso!");
-                    window.location.assign("../page_gerProdutos.php?id="+idVendedor);
-                })
-                .catch( error => {alert("Erro ao atualizar anúncio no banco de dados. ERRO: " + error)}) 
-            }
-        }
-
-        function exibirPreviaImagens() {
-            tbodyPrevImagens.innerHTML = ""; // Limpar o tbody da tabela de pré-visualização
-
-            // Obter os arquivos selecionados
-            let imagens = inputImagem.files;
-
-            for (let i = 0; i < imagens.length; i++) {
-                let imagem = imagens[i];
-
-                // Criar um elemento <img> com os atributos desejados
-                let img = document.createElement("img");
-                img.setAttribute("src", URL.createObjectURL(imagem));
-                img.setAttribute("width", "90");
-                img.setAttribute("height", "100");
-
-                // Criar a linha da tabela
-                let row = document.createElement("tr");
-                row.id = "prevImg-"+imagem.name;
-
-                // Criar a célula da imagem
-                let cellImagem = document.createElement("td");
-                cellImagem.classList.add("w3-center");
-                cellImagem.style = "vertical-align: middle;";
-                cellImagem.appendChild(img);
-                
-                // Criar célula de tamanho
-                let imgTamanhoMB = (imagem.size/1024/1024).toFixed(1);
-                let imgTamanhoKB = (imagem.size/1024).toFixed(1);
-                let cellTamanho  = document.createElement("td");
-                cellTamanho.classList.add("w3-center");
-                if(imgTamanhoMB > 1) {
-                    cellTamanho.classList.add("w3-bold");
-                    cellTamanho.classList.add("w3-text-red");
-                }
-                cellTamanho.style = "vertical-align: middle;";
-                cellTamanho.textContent = imgTamanhoMB < 0.1 ? imgTamanhoKB + " KB" : imgTamanhoMB + " MB";
-
-                // Criar célula de extensão
-                let extensao = imagem.name.replace(/^.*\.(.+)$/, '$1');
-                let formatosValidos = ["jpg", "jpeg", "gif", "png"];
-                let cellExtensaoArquivo = document.createElement("td");
-                cellExtensaoArquivo.classList.add("w3-center");
-                if(!formatosValidos.includes(extensao)) {
-                    cellExtensaoArquivo.classList.add("w3-bold");
-                    cellExtensaoArquivo.classList.add("w3-text-red");
-                }
-                cellExtensaoArquivo.style = "vertical-align: middle;";
-                cellExtensaoArquivo.textContent =  extensao;
-
-                // Criar a célula do botão de exclusão
-                let cellExcluir = document.createElement("td");
-                let btnExcluir = document.createElement("input");
-                cellExcluir.classList.add("w3-center");
-                cellExcluir.style = "vertical-align: middle;";
-                btnExcluir.type = "button";
-                btnExcluir.value = "Excluir";
-                btnExcluir.classList.add("w3-btn", "w3-theme", "w3-red");
-                btnExcluir.addEventListener("click", function () {
-                    excluirImagem(imagem.name);
-                });
-                cellExcluir.appendChild(btnExcluir);
-
-                // Adicionar as células à linha da tabela
-                row.appendChild(cellImagem);
-                row.appendChild(cellTamanho);
-                row.appendChild(cellExtensaoArquivo);
-                row.appendChild(cellExcluir);
-
-                // Adicionar a linha ao tbody da tabela de pré-visualização
-                tbodyPrevImagens.appendChild(row);
-            }
-        }
-
-        function excluirImagem(nomeArquivo) {
-            // Criar uma nova lista de arquivos sem o arquivo a ser excluído
-            let novosArquivos = new DataTransfer();
-
-            for (let i = 0; i < inputImagem.files.length; i++) {
-                if (inputImagem.files[i].name !== nomeArquivo) {
-                    novosArquivos.items.add(inputImagem.files[i]);
-                }
-            }
-
-            // Atribuir a nova lista de arquivos ao input de imagem
-            inputImagem.files = novosArquivos.files;
-
-            // Remover a linha da tabela de pré-visualização
-            let deletedRow = document.getElementById("prevImg-"+nomeArquivo);
-            deletedRow.remove();
-        }
-        
-        function confirmarCancelamento(){
-            showModalConfirmacao(
-                "<i class=\"w3-text-amber fa fa-solid fa-exclamation-triangle\"></i> &nbsp;",
-                "Você tem certeza?",
-                "Ao confirmar, todas as alterações não salvas serão perdidas. ",
-                "",
-                "w3-boder-amber",
-                "Sim",
-                "Não"
-            );
-
-            $("#btnPrimario-modalDeNotificao").off();
-            $("#btnPrimario-modalDeNotificao").on("click", function() { window.location.href="../page_gerProdutos.php" });
-        }
-
-        function confirmarAlteracao(){
-            showModalConfirmacao(
-                "<i class=\"w3-text-amber fa fa-solid fa-exclamation-triangle\"></i> &nbsp;",
-                "Você tem certeza?",
-                "Ao confirmar, todos os dados salvos anteriormente serão substituídos pelos novos dados inseridos. ",
-                "",
-                "w3-boder-amber",
-                "Sim",
-                "Não"
-            );
-
-            $("#btnPrimario-modalDeNotificao").off();
-            $("#btnPrimario-modalDeNotificao").on("click", function() { enviarFormulario() });
-        }
-
-    </script>
+    <script src="../scripts/script2_alterProduto.js"></script>
+    <script src="../scripts/validacoesProduto.js"></script>
 
 </body>
 
