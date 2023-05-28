fb605ce09f6512fb4d8b40ca122cc4dd9a5a7661
Carlos Krefer <carloskrefer@hotmail.com>
2023-05-28 19:57:43 -0300

    Corrige link ao clicar no nome

--- index.php
+++ index.php
@@ -105,12 +105,24 @@ nav { background-color: #3C486B!important; }
     <p class="w3-left">Camisetas</p>
     <p class="w3-right">
       <?php
-        // Exibe nome do usuário logado e o botão de logout, se não mostrará os botões de login e cadastro
+      // Exibe nome do usuário logado e o botão de logout, se não mostrará os botões de login e cadastro
+      // Também controla o link exibido ao clicar no próprio nome, após logado. Se for comprador, vai para a
+      // página de gerenciamento de comprador. Se for vendedor, para página de gerenciamento de produtos.
         $usuarioLogou = isset($_SESSION ['nome_usuario']);
+        $linkAoClicarNoNome = "/chestplace/index.php";
         if ($usuarioLogou) {
-          $nomeUsuario = $_SESSION['nome_usuario'];
+          $nomeUsuario = $_SESSION['nome_usuario'];   
+          if (isset($_SESSION['tipo_usuario'])) {
+            if ($_SESSION['tipo_usuario'] == 'comprador') {
+              $linkAoClicarNoNome = "/chestplace/page_gerComprador.php";
+            } else if ($_SESSION['tipo_usuario'] == 'vendedor') {
+              $linkAoClicarNoNome = "/chestplace/page_gerProdutos.php";  
+            } else if ($_SESSION['tipo_usuario'] == 'administrador') {
+              $linkAoClicarNoNome = "/chestplace/index.php"; // TODO: quando criar página de administrador, colocar o link correto aqui.
+            }
+          }
           echo <<<END
-            <span style="margin-right: 10px;">Olá,<a href="/chestplace/page_gerProdutos.php " > $nomeUsuario</a> </span>
+            <span style="margin-right: 10px;">Olá,<a href="$linkAoClicarNoNome" > $nomeUsuario</a> </span>
             <button class="w3-btn w3-deep-orange w3-border" onclick="window.location.href='./logout.php'" 
             style="font-size: 15px; font-weight: 700; margin-right: 10px;">Sair</button>
           END;
