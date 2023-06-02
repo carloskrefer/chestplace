b59e32711523b48ac9285f04b7cb001823e749cd
Carlos Krefer <carloskrefer@hotmail.com>
2023-06-01 21:23:30 -0300

    Adiciona modal de confirmação de exclusão

--- page_gerComprador.php
+++ page_gerComprador.php
@@ -27,6 +27,7 @@
 <body>
   <!-- Import dos elementos visuais -->
   <?php require("./common/header.php")?>
+  <?php require("./common/modalConfirmacao.php")?>
 
   <!-- !PAGE CONTENT! -->
   <div class="w3-main w3-content" style="margin-bottom:10vh;max-width:1200px">
@@ -53,14 +54,35 @@
     </a>
 
     <!-- BOTÃO DE EXCLUIR CONTA -->
-    <a href="" style="margin:auto;"> <!-- TODO: exibir modal de confirmação ao clicar -->
-      <div class="w3-center w3-border w3-border-black w3-round-xlarge w3-hover-border-orange" 
-        style="width: 400px; margin:auto; margin-top:50px; border-width: 16px!important;">
-      <i class="fa fa-exclamation-triangle" style="font-size: 150px; color: orange;"></i>
-      <p id="txtAltCad" style="font-size: 40px; margin:0px; font-weight: 900;">Excluir conta</p>
-      </div>
-    </a>
-    <div id="itens-publicado" class="w3-row">
+    <!-- cursor:pointer irá mudar o tipo do ponteiro do mouse quando passar por cima do botão. -->
+    <!-- O evento clicarBotaoExcluir() está no 'script_delComprador.js', que chama showModalConfirmacao() do 'modalConfirmacao.js'. -->
+    <script>
+      function excluirConta(){
+        showModalConfirmacao(
+            "<i class=\"w3-text-amber fa fa-solid fa-exclamation-triangle\"></i> &nbsp;",
+            "Você tem certeza?",
+            "Ao confirmar, a sua conta será excluída e você não poderá mais utilizar o seu login. ",
+            "",
+            "w3-boder-amber",
+            "Sim",
+            "Não"
+        );
+        $("#btnPrimario-modalDeNotificacao").on("click",function(){
+            // usar uma variável de sessão pra confirmar que o usuário realmente quer ir lá, pra não usar POST (pois pode chegar lá por GET)
+            //window.location.href='./actions/delComprador_exe.php';
+            document.getElementById("delCompradorForm").submit();
+        });
+      }
+    </script>
+    <form id="delCompradorForm" action="./actions/delComprador_exe.php" method="post">
+        <div class="w3-center w3-border w3-border-black w3-round-xlarge w3-hover-border-orange" 
+          style="width: 400px; margin:auto; margin-top:50px; border-width: 16px!important; cursor:pointer;"
+          onclick="excluirConta()">
+        <i class="fa fa-exclamation-triangle" style="font-size: 150px; color: orange;"></i>
+        <p id="txtAltCad" style="font-size: 40px; margin:0px; font-weight: 900;">Excluir conta</p>
+        </div>
+        <input type="submit" style="display:none;">
+    <form>
   
   <!-- End page content -->
     <div id="teste" style=" z-index:100; position: fixed; bottom: 0; left: 0; width: 100%;" class="w3-black w3-center w3-padding">
