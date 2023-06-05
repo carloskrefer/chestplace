58601141e1c75d1821f689fcfd06033271ab8e31
Gabriel <gabrielgmns071204@gmail.com>
2023-06-04 22:26:44 -0300

    Implementado: pesquisa por data + comentarios

--- page_gerVendas.php
+++ page_gerVendas.php
@@ -13,7 +13,7 @@
     // Utilizado para definir os botões do header
     $tipoPagina = "gerenciarVendasVendedor";
 
-    $filtrosAceitos = ["ID", "PRODUTO", "COMPRADOR"];
+    $filtrosAceitos = ["ID", "PRODUTO", "COMPRADOR", "DCOMPRA"];
     $orderAceitos   = ["ID", "PRODUTO", "VALOR", "COMPRADOR", "DCOMPRA" , "STATUS"];
 
 
@@ -22,16 +22,94 @@
     if(!isset($_GET["orderBy"]) || !in_array($_GET["orderBy"], $orderAceitos)) $_GET["orderBy"] = "";
     if(!isset($_GET["sentido"])) $_GET["sentido"] = "";
 
-    // Define o filto de pesquisa
+    /**
+     * 
+     *         FILTROS DE PESQUISA DE VENDAS
+     * 
+     * Para adicionar novos métodos de pesquisa (BARRA DE PESQUISA):
+     * 
+     * 1- Adicionar clausula no enumFiltro
+     * 
+     * 2- Adicionar option no selectBox pesquisarPor
+     * 
+     * 3- Adicionar cláusula no array de filtrosAceitos
+     * 
+     * 
+     *         ORDENAÇÃO
+     * 
+     * Para adicionar novos métodos de ordenação (orderBy):
+     * 
+     * 1- Adicionar cláusula no enumOrderBy
+     * 
+     * 2- Adicionar cláusula no array de orderAceitos
+     * 
+     * 3- Adicionar o evento onclick com a função ordernar() na tag de <th></th> da coluna que a tabela será ordenado:
+     *     3.1 - O parâmetro do ordenar é o da cláusula ENUM(), por exemplo:
+     *           ordernar por "VALOR" vai ser onclick="ordenar('VALOR').
+     *           "VALOR" está na chave do enum:
+     *           "VALOR" => " ORDER BY c.preco * cv.quantidade"
+     * 
+     * 4- Adicionar as tags com os símbolos de ordenação (ASC/DESC):
+     * 
+     *      <i class="fa-sharp fa-solid fa-sort-down ord" id="ORDENACAO-ASC"></i>
+     *      <i class="fa-sharp fa-solid fa-sort-up ord" id="ORDENACAO-DESC"></i>
+     *      
+     *      ORDENACAO deve ser substituído pela chave do enum que você adicionou anteriormente:
+     *      
+     * 5- Caso a <th></th> correspondente ao dado a ser ordenado não esteja sendo exibida,
+     *     é necessário colocá-la também. Exemplo: dataHoraEntrega não está sendo exibida, logo
+     *     um <th>Data Hora Compra</th> na primeira linha da tabela.
+     * 
+     *     @example 
+     *     EXEMPLO para ordenar por dataHoraEntrega
+     * 
+     *          > adicionado ao enumOrderBy: "DHENTREGA" => "CLAUSULA ORDER BY DO SELECT"
+     * 
+     *          > adicionado onclick ao <th>Data Hora Entrega</th>: <th onclick="ordernar("DHENTREGA")">Data Hora Entrega</th>
+     *      
+     *          > adicionado ícones de sentido de ordenação: 
+     *              <i class="fa-sharp fa-solid fa-sort-down ord" id="DHENTREGA-ASC"></i>
+     *              <i class="fa-sharp fa-solid fa-sort-up ord"   id="DHENTREGA-DESC"></i>
+    */
+
+
+    // Faz um array com todas as letras da pesquisa. Exemplo: "camiseta A" fica: ["c","a","m","i","s","e","t","a","A"]
     $letrasDaPesquisa = str_split(str_replace(" ","",$_GET["pesquisa"]));
+
+
+    /**
+     * ENUM com o tipo de filtro que será feito e a instrução WHERE do select que será feito depois
+     * a instrução é iniciada com AND e depois onde a instrução sobre o que deve ser parecido com o que.
+     * Exemplo:
+     * 
+     * pesquisarPor : "cam"
+     * 
+     * Produto: vai pesquisar os registros onde c.titulo (título da camiseta) é parecido com:
+     * 
+     * "%c%" AND c.titulo LIKE "%a%" AND c.titulo LIKE "%m%"
+     * 
+     * - a '%' no início e no fim simboliza procurar por aquela string em qualquer posição do texto
+     *   não necessariamente no fim ou no início da string
+     * 
+     * - o 'impode' coloca cada um dos itens de uma array separados por uma determinada string
+     *   nesse caso, "%' AND c.nome LIKE '%".
+     * 
+     * - o primeiro e o último %, juntamente com o primeiro LIKE, já estão na string. Assim, caso só haja uma letra, o resultado será um úncio
+     *   LIKE só com a primeira letra. 
+     */
     $enumFiltro = array(
         "ID" => " AND cv.id LIKE '%".implode("%' AND cv.id LIKE '%", $letrasDaPesquisa)."%'",
         "PRODUTO" => " AND c.titulo LIKE '%".implode("%' AND c.titulo LIKE '%", $letrasDaPesquisa)."%'",
         "COMPRADOR" => " AND u.nome LIKE '%".implode("%' AND u.nome LIKE '%", $letrasDaPesquisa)."%'",
+        "DCOMPRA" => " AND cv.data_hora_compra LIKE '%".implode("%' AND cv.data_hora_compra LIKE '%", $letrasDaPesquisa)."%'",
         NULL => ""
     );
 
-    // Define a ordenação da pesquisa
+    /**
+     * ENUM que define a ordenação da pesquisa (será passado por GET depois)
+     * 
+     * a cláusula será adiciona depois na query SQL
+     */
     $enumOrderBy = array(
         "ID" => " ORDER BY cv.id",
         "PRODUTO" => " ORDER BY c.titulo",
@@ -83,6 +161,7 @@
                     <option value="ID" <?= ($_GET["pesquisarPor"] == "ID") ? "selected" : "" ?>>ID</option>
                     <option value="PRODUTO" <?= ($_GET["pesquisarPor"] == "PRODUTO") ? "selected" : "" ?>>PRODUTO</option>
                     <option value="COMPRADOR" <?= ($_GET["pesquisarPor"] == "COMPRADOR") ? "selected" : "" ?>>COMPRADOR</option>
+                    <option value="DCOMPRA" <?= ($_GET["pesquisarPor"] == "DCOMPRA") ? "selected" : "" ?>>DATA DA COMPRA</option>
                 </select>
                 <input onkeydown="if (event.key === 'Enter') { pesquisar(); }" type="text" name="pesquisa" id="pesquisa" class="w3-input w3-border w3-large w3-round-large" style="width:75%;" placeholder="Insira o termo a ser pesquisado" value="<?= ($_GET["pesquisa"] != "null" && $_GET["pesquisa"] != null) ? $_GET["pesquisa"] : ""; ?>">
                 <button onclick="pesquisar();" class="w3-button w3-round-xxlarge w3-blue" type="submit">
@@ -129,10 +208,15 @@
         </thead>
         <tbody>
             <?php
+                // Se é uma clásula de pesquisa válida (é uma chave no enumFiltro), o valor passa a ser a query pega no enumFiltro(valor), caso contrário, é vazio.
                 $filtro = array_key_exists($_GET["pesquisarPor"], $enumFiltro) ? $enumFiltro[$_GET["pesquisarPor"]] : "";
+                // Se é uma cláusula de ordenação válida (é uma chave no enumOrderBy), valor passa a ser a query de ordenação (valor), caso contrário, vazio.
                 $ordenacao = array_key_exists($_GET["orderBy"], $enumOrderBy) ? $enumOrderBy[$_GET["orderBy"]] : "";
+                // (ASC/DESC) se o sentido está settado e se o valor dele é ASC ou DESC e ordenação (variavel de cima) não é vazia
                 $sentido   = isset($_GET["sentido"]) && ($_GET["sentido"] == "ASC" || $_GET["sentido"] == "DESC") && $ordenacao != "" ? " ".$_GET["sentido"] : "";
                 
+
+                // Query selecionando todos os dados necessários para mostrar os dados da venda
                 $selectVendasQuery = 
                 "SELECT 
                     cv.id,
@@ -161,10 +245,14 @@
                     $sentido .
                     ($sentido != "" ? ", u.nome ASC;" : "")
                     ;
-                
+                // Aqui no final coloca o filtro de pesquisa, a ordenção e o sentido.
+
+
+                // Resultado do select dos dados das vendas
                 $resultVendas = mysqli_query($conn, $selectVendasQuery);
 
 
+                // Percorrendo todos os dados do SELECT de vendas
                 while($venda = mysqli_fetch_assoc($resultVendas)){
 
                     // if(is_null($venda["data_hora_confirmacao_pagamento"])){
@@ -175,8 +263,10 @@
                     //     $status = "Pedido entregue ao destinatário.";
                     // }
 
+                    // Calcula o TOTAL quantidade * preço unitário
                     $subtotal = number_format(floatval($venda["preco"])*floatval($venda["quantidade"]),2,",",".");
 
+                    // Printa os dados na tabela
                     echo "
                     <tr>
                         <td class=\"w3-center\">".$venda["id"]."</td>
