dc07d72090384d8ed3764d390b7bbfaebd656ce8
Gabriel <gabrielgmns071204@gmail.com>
2023-06-05 21:26:28 -0300

    Implementado: action de compra

--- actions/comprar_exe.php
+++ actions/comprar_exe.php
@@ -13,13 +13,24 @@
         redirect("../page_gerProdutos.php");
     }
 
-    $idCamiseta = $_POST["idCamiseta"];
-    $quantidade = $_POST["quantidade"];
+    $idCamiseta = $_POST["idCamiseta"];  
     
-    $insertQuery = "INSERT INTO compra_venda (id_camiseta, id_comprador, data_hora_compra, data_hora_confirmacao_pagamento,data_hora_recebimento,quantidade) VALUES ($idCamiseta, ".$_SESSION["id_usuario"].", NOW(), NULL, NULL, $quantidade);";
-
+    mysqli_begin_transaction($conn);
     try{
-        mysqli_query($conn, $insertQuery);
+        // SELECT dos tamanhos
+        $resultTam = mysqli_query($conn, "SELECT * FROM tamanho");
+
+        // Para cada tamanho
+        while($row = mysqli_fetch_assoc($resultTam)){
+            $qtdeSelect = isset($_POST["quantidade_".$row["codigo"]]) ? $_POST["quantidade_".$row["codigo"]] : null;
+            if(!is_null($qtdeSelect)){
+                // Insert na compra_venda;
+                $insertQuery = "INSERT INTO compra_venda (id_camiseta, id_comprador, data_hora_compra, data_hora_confirmacao_pagamento,data_hora_recebimento,quantidade) VALUES ($idCamiseta, ".$_SESSION["id_usuario"].", NOW(), NULL, NULL, ".$qtdeSelect.");";
+                mysqli_query($conn, $insertQuery);
+            }
+        }
+
+        
     } catch (Exception $e){
         alert("Houve um erro ao comprar o produto: " . $e->getMessage());
     }
